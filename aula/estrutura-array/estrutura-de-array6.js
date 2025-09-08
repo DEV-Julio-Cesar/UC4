667a8b89e8a85e693546f884bffa5d4f4569e6cc
koldrinegao <144956386+koldrinegao@users.noreply.github.com>
2025-09-08 18:40:37 -0300

    atualização das atividades

---
+++ aula/estrutura-array/estrutura-de-array6.js
@@ -0,0 +1,19 @@
+import PromptSync from "prompt-sync" // Importe do prompt
+const prompt = PromptSync() // variavel do prompt
+
+let senhaEntrada1=[], senhaEntrada2=[], senhaEntrada3=[], senhaEntrada4=[]
+let contadorsenha
+
+
+senhaEntrada1 = prompt("Digite sua senha: ")
+senhaEntrada2 = prompt("Digite sua senha: ")
+senhaEntrada3 = prompt("Digite sua senha: ")
+senhaEntrada4 = prompt("Digite sua senha: ")
+
+
+
+
+console.log(senhaEntrada1)
+console.log(senhaEntrada2)
+console.log(senhaEntrada3)
+console.log(senhaEntrada4)

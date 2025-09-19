a7d59d260e8e68a4fad4169da0ddc27f9ff03482
koldrinegao <144956386+koldrinegao@users.noreply.github.com>
2025-09-18 21:26:37 -0300

    atualização

---
+++ aula/array-de-array/array.js
@@ -0,0 +1,34 @@
+import promptSync from 'prompt-sync'
+const prompt = promptSync()
+
+
+
+// [['pao', 'feijoada', 'sopa'], ['bolo'], []]
+let mat = [[],[],[],[],[],[],[],]
+mat[0] = ['pao', 'feijoada', 'sopa']
+// mat[1] = []
+// mat[1][0] = 'bolo'
+// mat[2] = []
+let cardapio = []
+let semana = ['segunda', 'terca','quarta','quinta','sexta','sabado','domingo']
+let turno = ['cafe', 'almoco', 'jantar']
+for(let i = 0; i < 7; i++){// dias da semana
+    cardapio[i] = []
+    for(let j = 0; j < 3; j++ ){//turnos (café, almoço, jantar)
+        cardapio[i][j] = prompt(`Digite sua opção ${semana[i]} - ${turno[j]}: `)
+      //  cardapio[i].push(prompt(`Digite sua opção ${semana[i]} - ${turno[j]}: `))
+    }
+}
+console.table(cardapio) // mostra no formato tabela
+
+//cardapio[0].splice(0, 1) // remoção com busca
+
+//cardapio[0].splice(cardapio[0].indexOf('pao'), 1) // remoção com busca
+
+//console.log(cardapio)
+
+//cardapio[1][1] = 'Feijao e Arroz' // alteração de valor
+
+//console.log(cardapio)
+
+// CRUD - CREATE READ UPDATE DELETE
---
+++ aula/array-de-array/atividade1.js
@@ -0,0 +1,51 @@
+import promptSync from 'prompt-sync'
+const prompt = promptSync()
+
+let cardapio = []
+let semana = ['segunda', 'terca','quarta','quinta','sexta','sabado','domingo']
+let turno = ['cafe', 'almoco', 'jantar']
+for(let i = 0; i < 7; i++){// dias da semana
+    cardapio[i] = []
+    for(let j = 0; j < 3; j++ ){//turnos (café, almoço, jantar)
+        cardapio[i][j] = prompt(`Digite sua opção ${semana[i]} - ${turno[j]}: `)
+      //  cardapio[i].push(prompt(`Digite sua opção ${semana[i]} - ${turno[j]}: `))
+    }
+}
+console.table(cardapio) // mostra no formato tabela
+
+let dia = prompt(`Digite sua opção para substituir cadapio digite o dia: `).toLowerCase()
+let refeicao = prompt(`Digite sua opção para substituir cadapio digite a refeição: `).toLowerCase()
+cardapio[0].splice(cardapio[0].indexOf('pao'), 1) // remoção com busca
+let i = semana.splice(dia)
+let j = turno.splice(refeicao)
+switch(refeicao){
+    case "cafe":
+        cardapio[i][j] = prompt ("remova o cafe")
+        break
+        case "almoco":
+        cardapio[[i]][[j]] = prompt ("remova o almoço")
+        break
+        case "jantar":
+        cardapio[[i]][[j]] = prompt ("remova a janta")
+        break
+}
+console.table(cardapio)
+
+
+let dia = prompt(`Digite sua opção para substituir cadapio digite o dia: `).toLowerCase()
+let refeicao = prompt(`Digite sua opção para substituir cadapio digite a refeição: `).toLowerCase()
+let i = semana.indexOf(dia)
+let j = turno.indexOf(refeicao)
+switch(refeicao){
+    case "cafe":
+        cardapio[i][j] = prompt ("substitua a refeição desejada do cafe")
+        break
+        case "almoco":
+        cardapio[[i]][[j]] = prompt ("substitua a refeição desejada do almoço")
+        break
+        case "jantar":
+        cardapio[[i]][[j]] = prompt ("substitua a refeição desejada da janta")
+        break
+}
+console.table(cardapio)
+
